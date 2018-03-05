9b07e735e7b7a8ec79c3a50b12141a40a12c2768
Amirreza Amouie <aamouie01@outlook.com>
2018-03-05 08:09:24 +0330

    Add the words to a file

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ start = time.time()
 print("Reading scripts...")
 filereader.read()
 end = time.time()
+
+words_file = open('answer.txt', 'w')
 for word in filereader.words_list:
     print(word)
+    words_file.write("%s\n" % word)
 
 print("\n\n### TOTAL LOADING TIME:", end - start, "\n")
